3b198279e036b18a1eb00c210cb3bec94d53fdb7
jpcarmona <jpcarmona92@gmail.com>
2018-03-04 21:17:47 +0100

    Avance1 Funciones

---
+++ funciones.py
@@ -0,0 +1,38 @@
+#1.Listar ciudades y radares
+
+def list_ciu_radar(arbol1,arbol2):
+	dict1={}
+	codprov1=arbol1.xpath('//PROVINCIA/NOMBRE/text()')
+	codprov2=arbol2.xpath('//provincia/@id')	
+	for cod2 in codprov2:
+		cod1=cod2
+		prov=arbol2.xpath('//provincia[@id="{}"]/text()'.format(cod2))
+		nomprov=prov[0]
+		carreteras=arbol1.xpath('//PROVINCIA[NOMBRE="{}"]/CARRETERA/DENOMINACION/text()'.format(cod1))
+		dict1[nomprov[0]]={}
+		for carretera in carreteras:
+			dict1[nomprov][carretera]={}
+			dict1[nomprov][carretera]["PI"]={}
+			pipk=arbol1.xpath('//PROVINCIA[NOMBRE="{}"]/CARRETERA[DENOMINACION="{}"]/RADAR/PUNTO_INICIAL/PK/text()'.format(cod1,carretera))
+			dict1[nomprov][carretera]["PI"]["PK"]=pipk
+			pilat=arbol1.xpath('//PROVINCIA[NOMBRE="{}"]/CARRETERA[DENOMINACION="{}"]/RADAR/PUNTO_INICIAL/LATITUD/text()'.format(cod1,carretera))
+			dict1[nomprov][carretera]["PI"]["LAT"]=pilat
+			pilon=arbol1.xpath('//PROVINCIA[NOMBRE="{}"]/CARRETERA[DENOMINACION="{}"]/RADAR/PUNTO_INICIAL/LONGITUD/text()'.format(cod1,carretera))
+			dict1[nomprov][carretera]["PI"]["LON"]=pilon
+			dict1[nomprov][carretera]["PF"]={}
+			pfpk=arbol1.xpath('//PROVINCIA[NOMBRE="{}"]/CARRETERA[DENOMINACION="{}"]/RADAR/PUNTO_FINAL/PK/text()'.format(cod1,carretera))
+			dict1[nomprov][carretera]["PF"]["PK"]=pfpk
+			pflat=arbol1.xpath('//PROVINCIA[NOMBRE="{}"]/CARRETERA[DENOMINACION="{}"]/RADAR/PUNTO_FINAL/LATITUD/text()'.format(cod1,carretera))
+			dict1[nomprov][carretera]["PF"]["LAT"]=pflat
+			pflon=arbol1.xpath('//PROVINCIA[NOMBRE="{}"]/CARRETERA[DENOMINACION="{}"]/RADAR/PUNTO_FINAL/LONGITUD/text()'.format(cod1,carretera))
+			dict1[nomprov][carretera]["PF"]["LON"]=pflon
+	return dict1
+
+
+#2.Contar ciudades y radares
+
+#3.Buscar ciudades por la que pase una carretera y muestre radares
+
+#4.Buscar una ciudad y muestra sus radares, Busca una carretera y muestra sus radares
+
+#5.Mostrar las coordenadas de los radares que tienen una carretera
